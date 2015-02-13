157343881a8c04c7fe138ef4d734f0f131694a8b
2DAWT <2DAWT@T4PC01>
2015-02-13 20:50:08 +0100

    u

---
+++ Pruebas/10.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="ISO-8859-1">
+</head>
+<body>
+<?php  
+
+$num = 5;
+
+for ($mult = 1; $mult < 11; $mult++)
+{
+	
+	echo $mult * $num . "<br>";
+	
+}
+
+?>
+
+</body>
+</html>
---
+++ Pruebas/11.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta charset="ISO-8859-1">
+</head>
+<body>
+<?php  
+
+ for ($num = 1; $num < 11; $num++)
+ {
+
+for ($mult = 1; $mult < 11; $mult++)
+{
+	
+	echo $mult * $num . "<br>";
+	
+}
+echo "<br>";
+ }
+?>
+
+</body>
+</html>
